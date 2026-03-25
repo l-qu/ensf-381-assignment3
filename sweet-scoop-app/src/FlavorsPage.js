cf306992ea90fcf51865e74d757f48dd1383ab5c
gianna-k0520 <gianna.kong@ucalgary.ca>
2026-03-24 21:38:14 -0600

    Removed my footer and header and also updated flavourspage and orderlist

--- sweet-scoop-app/src/FlavorsPage.js
+++ sweet-scoop-app/src/FlavorsPage.js
@@ -5,12 +5,33 @@ import FlavorCatalog from "./components/FlavorCatalog";
 import OrderList from "./components/OrderList";
 
 function FlavorPage() {
+    const [order, setOrder] = useState([]);
+
+    const handleAdd = (flavor) => {
+        setOrder((prev) => {
+            const existing = prev.find(item => item.id === flavor.id);
+
+            if(existing){
+                return prev.map(item =>
+                    item.id === flavor.id ? {...item, quantity: item.quantity + 1} : item
+                );
+            }
+            return [... prev, {...flavor, quantity: 1}];
+        });
+    };
+
+    const handleRemove = (id) => {
+        setOrder((prev) => prev.map(item =>
+            item.id === id ? {...item, quantity: item.quantity - 1} : item
+        ).filter(item => item.quantity > 0));
+    };
+
     return (
         <div className="flavors-page">
             <Header />
             <div className="content">
-                <FlavorCatalog />
-                <OrderList />
+                <FlavorCatalog onAdd = {handleAdd} />
+                <OrderList order = {order} onRemove = {handleRemove} />
             </div>
             <Footer />
         </div>
@@ -18,4 +39,3 @@ function FlavorPage() {
 }
 
 export default FlavorPage
-
--- sweet-scoop-app/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-function Footer(){
-    return(
-        <footer>
-            <p>&copy; 2026 Sweet Scoop Ice Cream.</p>
-        </footer>
-    )
-}
-
-export default Footer
